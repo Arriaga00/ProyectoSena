ebdeaa4bd35b81a76a7451fa85e8fa9057779a62
andres arriaga <andresfelipearriaga@gmail.com>
2024-02-13 00:05:00 -0500

    primer commit

---
+++ src/main.js
@@ -0,0 +1,10 @@
+ const d = document
+
+ const contenedorTarjetas = d.getElementById('contenedor')
+ const btnDerecha = d.getElementById('derecha')
+ const btnIzquierda = d.getElementById('izquierda')
+
+ d.addEventListener('click',(e)=>{
+    if(e.target===btnDerecha) contenedorTarjetas.scrollLeft += 400
+    if(e.target===btnIzquierda) contenedorTarjetas.scrollLeft -= 400
+ })
